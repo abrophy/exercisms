3d4ae23112d83ef601ec563c8c99a5c6c997675a
Arthur Brophy <artbrophy@gmail.com>
2016-11-02 22:30:06 -0400

    completed rna_transcription exercism

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -3,4 +3,25 @@ module BookKeeping
 end
 
 class Complement
+  def self.of_dna(strand)
+    complements = {
+      :G => 'C',
+      :C => 'G',
+      :T => 'A',
+      :A => 'U',
+    }
+
+    strand_array = strand.split('')
+
+    strand_array.map! do |item|
+      if complements.include? item.to_sym
+        complements[item.to_sym]
+      else
+        strand_array = [""]
+        break
+      end
+    end
+
+    strand_array.join ''
+  end
 end
