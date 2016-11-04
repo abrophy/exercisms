fbfecd07794982aeb70b4349465acdf2dbe2baf8
Arthur Brophy <artbrophy@gmail.com>
2016-11-03 22:49:22 -0400

    completed hacky solution for raindrops

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -3,4 +3,33 @@ module BookKeeping
 end
 
 class Raindrops
+  def self.convert(number)
+    converted = false
+    output = number
+
+    if number % 3 == 0
+      output = 'Pling'
+      converted = true
+    end
+
+    if number % 5 == 0
+      if converted
+        output += 'Plang'
+      else
+        converted = true
+        output = 'Plang'
+      end
+    end
+
+    if number % 7 == 0
+      if converted
+        output += 'Plong'
+      else
+        converted = true
+        output = 'Plong'
+      end
+    end
+
+    output.to_s
+  end
 end
