68844f999cb06f23d4017ab8054d2a9489207dd6
Arthur Brophy <artbrophy@gmail.com>
2016-11-06 01:43:43 -0500

    submitted unoptimized solution to difference-of-squares ruby exercism

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -3,4 +3,32 @@ module BookKeeping
 end
 
 class Squares
+  def initialize(number)
+    @given_number = number
+  end
+
+  def square_of_sum
+    i = 0
+    total = 0
+    @given_number.times do
+      i += 1
+      total += i
+    end
+    total ** 2
+  end
+
+  def sum_of_squares
+    i = 0
+    total = 0
+    @given_number.times do
+      i += 1
+      total += i**2
+    end
+    total
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+
 end
