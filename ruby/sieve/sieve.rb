b501aafc1926383a7fc06a26206eb57c50aea86d
Arthur Brophy <artbrophy@gmail.com>
2017-01-02 16:03:57 -0500

    fixed result for 2

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -9,12 +9,18 @@ class Sieve
   end
 
   def primes
-
     if @given_number < 2
       # returns an empty list as there are no primes under 2
       []
+    elsif @given_number == 2
+      [2]
     else
+      eratosthenes
     end
+  end
+
+  def eratosthenes
+
   end
 
 end
