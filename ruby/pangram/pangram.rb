bd9d5411f73ff7b89eb3ce3f1af3aa79a7cd19e6
Arthur Brophy <artbrophy@gmail.com>
2017-01-01 21:08:30 -0500

    created the boilerplate pangram file

---
+++ ruby/pangram/pangram.rb
@@ -0,0 +1,16 @@
+module BookKeeping
+  #VERSION = 3
+end
+
+class Pangram
+
+  def initialize(string)
+    @given_string = string
+  end
+
+  def self.is_pangram? string
+    # place holder return false by default
+    false
+  end
+
+end
