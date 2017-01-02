04683c4104d957cbf8742db51bbfcd46788d37f4
Arthur Brophy <artbrophy@gmail.com>
2017-01-02 15:56:12 -0500

    created basic solution boilerplate for exercise

---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,20 @@
+module BookKeeping
+  #VERSION = 2
+end
+
+class Sieve
+
+  def initialize(number)
+    @given_number = number
+  end
+
+  def primes
+
+    if @given_number < 2
+      # returns an empty list as there are no primes under 2
+      []
+    else
+    end
+  end
+
+end
