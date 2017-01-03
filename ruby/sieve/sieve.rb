f1023926711eee3bf50e96832b9363f856062ee2
Arthur Brophy <artbrophy@gmail.com>
2017-01-03 16:52:09 -0500

    completed messy brute force sieve of Eratosthenes

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -20,7 +20,33 @@ class Sieve
   end
 
   def eratosthenes
+    @array_of_numbers = []
+    @array_of_primality = []
+    (2..@given_number).each do |item|
+      @array_of_numbers.push(item)
+      @array_of_primality.push(true)
+    end
 
+    # loop through the values
+    @array_of_numbers.each do |item|
+      for i in item..@given_number
+        if i == item
+        elsif i % item == 0
+          @array_of_primality[i-2] = false
+        end
+      end
+    end
+
+    # dump the values
+    @result = []
+    @array_of_primality.each_with_index do |item,index|
+      if item
+        @result.push @array_of_numbers[index]
+      else
+      end
+    end
+
+    @result
   end
 
 end
