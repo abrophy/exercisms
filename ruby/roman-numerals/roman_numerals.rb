9f998e6c50c3caa8d86e3826e4714b7b0f1b48ed
Arthur Brophy <artbrophy@gmail.com>
2017-01-05 18:34:33 -0500

    attempt at solution,however string concat is failing

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -3,7 +3,51 @@ module BookKeeping
 end
 
 class Fixnum
+
   def to_roman
 
+  numerals = [
+    [1000, "M"],
+    [100, "C"],
+    [10, "X"],
+    [5, "V"],
+    [1, "I"],
+  ]
+
+    running_value = self
+    result_string = ""
+
+    numerals.each_with_index do |item, index|
+      if running_value >= item[0]
+        how_many = running_value/item[0].to_i
+        puts how_many.inspect
+        puts item[1].inspect
+        puts result_string.inspect
+        result_string += (how_many * item[1])
+        running_value -= how_many * item[0]
+
+        puts running_value
+        puts result_string
+
+      elsif running_value <= item[0]
+
+        puts running_value
+        puts result_string
+
+        unless item[0] == 1
+          if running_value >= (item[0] - numerals[index + 1][0])
+            result_string += numerals[index+1][0] + item[0]
+            running_value -= item[1] - numerals[index+1][1]
+          end
+        end
+      end
+
+      puts "CHECK"
+      puts running_value
+      puts result_string
+
+    end
+
+    result_string
   end
 end
