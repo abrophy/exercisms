60bf017df2ddf9fc18ad1804097ad78b02432c9d
Arthur Brophy <artbrophy@gmail.com>
2017-01-05 23:14:13 -0500

    it's a mess of hacky explicitness, but at least it works

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -8,7 +8,9 @@ class Fixnum
 
   numerals = [
     [1000, "M"],
+    [500, "D"],
     [100, "C"],
+    [50, "L"],
     [10, "X"],
     [5, "V"],
     [1, "I"],
@@ -20,34 +22,35 @@ class Fixnum
     numerals.each_with_index do |item, index|
       if running_value >= item[0]
         how_many = running_value/item[0].to_i
-        puts how_many.inspect
-        puts item[1].inspect
-        puts result_string.inspect
         how_many.times do
           result_string += item[1]
         end
         running_value -= how_many * item[0]
-
-        puts running_value
-        puts result_string
-
       elsif running_value <= item[0]
 
-        puts running_value
-        puts result_string
-
-        unless item[0] == 1
-          if running_value >= (item[0] - numerals[index + 1][0])
-            result_string += numerals[index+1][1] + item[1]
-            running_value -= item[0] - numerals[index+1][0]
+        unless item[0] == 1 || running_value == 5 || running_value == 10
+          next_along = numerals[index + 1]
+          if next_along[0] == 5 || next_along[0] == 50 || next_along[0] == 500
+            next_along = numerals[index + 2]
+          end
+          if running_value >= (item[0] - next_along[0])
+            result_string += next_along[1] + item[1]
+            running_value -= item[0] - next_along[0]
           end
         end
+
+        if item[0] == 5 && running_value == 5
+          # TODO
+          # create a more general case algo
+          result_string += "V"
+        end
+
+        if item[0] == 10 && running_value == 10
+          # TODO
+          # create a more general case algo
+          result_string += "X"
+        end
       end
-
-      puts "CHECK"
-      puts running_value
-      puts result_string
-
     end
 
     result_string
