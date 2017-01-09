1fbbff33a48a5e0ce0cb2c8823fd506eee256128
Arthur Brophy <artbrophy@gmail.com>
2017-01-09 14:30:27 -0500

    generating name successfully

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -3,7 +3,29 @@ module BookKeeping
 end
 
 class Robot
-  def name
-    #code goes here
+
+  attr_accessor :name, :used_names
+
+  def initialize
+    self.name = ""
+    self.used_names = []
   end
+
+  def name
+    name = generate_name
+    if self.used_names.index(name) != nil && self.used_names.index(name) > 0
+      self.name
+    else
+      self.used_names.push(name)
+      self.name = name
+      name
+    end
+  end
+
+  def generate_name
+    letters = ('A'..'Z').to_a
+    name = "" + letters[rand(26)] + letters[rand(26)] + rand(1000).to_s
+    name
+  end
+
 end
