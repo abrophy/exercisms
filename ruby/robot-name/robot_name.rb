7f87d3f600989578f794db1f2007224665adfdc8
Arthur Brophy <artbrophy@gmail.com>
2017-01-09 15:01:06 -0500

    refactored reset code into reset method

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -18,15 +18,19 @@ class Robot
     if self.current_name.length > 0
       self.current_name
     else
-      inner_name = generate_name
+      self.reset
+    end
+  end
 
-      if self.used_names.index(inner_name) != nil && self.used_names.index(inner_name) > 0
-        self.name
-      else
-        self.used_names.push(inner_name)
-        self.current_name = inner_name
-        inner_name
-      end
+  def reset
+    inner_name = generate_name
+
+    if self.used_names.index(inner_name) != nil && self.used_names.index(inner_name) > 0
+      self.reset
+    else
+      self.used_names.push(inner_name)
+      self.current_name = inner_name
+      inner_name
     end
   end
 
--- ruby/robot-name/robot_name_test.rb
+++ ruby/robot-name/robot_name_test.rb
@@ -27,13 +27,11 @@ class RobotTest < Minitest::Test
   end
 
   def test_different_robots_have_different_names
-    skip
     Kernel.srand DIFFERENT_ROBOT_NAME_SEED
     refute_equal Robot.new.name, Robot.new.name
   end
 
   def test_reset_name
-    skip
     Kernel.srand DIFFERENT_ROBOT_NAME_SEED
     robot = Robot.new
     name = robot.name
